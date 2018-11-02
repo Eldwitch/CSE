3357bef83af3585bb5ef3794b1cb6603347c3637
Eldwitch <42391782+Eldwitch@users.noreply.github.com>
2018-11-02 14:35:40 -0700

    finna hang a man
    yeet

--- notes/Everett Garrison - Guess Game.py
+++ notes/Everett Garrison - Guess Game.py
@@ -1,5 +1,13 @@
 import random
 random = (random.randint(1, 10))
-print(random)
-guess1 = input("Guess a number, any number: ")
 
+for i in range(5):
+    guess = int(input("Guess a number, any number: "))
+    if guess < random:
+        print("Guess is too low.")
+    elif guess > random:
+        print("Guess is too high.")
+    elif guess == random:
+        print()
+        print("You win!")
+        break
---
+++ notes/Everett Garrison - Hangman.py
@@ -0,0 +1,1 @@
+# finna hang a man up in this bitch... YEET
