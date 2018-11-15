55ae1be35b6d43a3c599d6739ad08e87aa4b78e3
Eldwitch <42391782+Eldwitch@users.noreply.github.com>
2018-11-15 14:35:51 -0800

    AAAAAH

--- notes/Everett Garrison - Hangman.py
+++ notes/Everett Garrison - Hangman.py
@@ -1,3 +1,15 @@
 import random
-words = random.list("bepis")
+Words = ["bepis"]
+""", "singular", "bees", "screaming", "satan", "god", "hello",
+         "why", "nine", "zoink", "taste", "swivel", "egg", "thirsty", "fifteen",
+         "joke", "scoobis", "demon", "ech", "store"""""
+Alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",
+            "p", "q", "r", "s", "t", "u", "v","w", "x", "y", "z"]
+word = random.randint(0, 19)
+random_word = Words[word]
+print(random_word)
+guess = 6
 
+if guess > 0:
+    if Words == "bepis":
+        input("Guess a letter ")
