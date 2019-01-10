65b99d43c6ea8cb2da1c47fa9b17157bd5b9f5a3
Eldwitch <42391782+Eldwitch@users.noreply.github.com>
2019-01-10 14:37:11 -0800

    ofrhsdgjrjldfn

--- Everett Garrison - Hangman.py
+++ Everett Garrison - Hangman.py
@@ -1,13 +1,11 @@
 import random
-Words = ["bepis", "singular", "bees", "screaming", "satan", "god", "hello",
+Words = ["singular", "bees", "SCREAMING", "satan", "god", "hello",
          "why", "nine", "zoink", "taste", "swivel", "egg", "thirsty", "fifteen",
-         "joke", "scoobis", "demon", "ech", "store"]
+         "joke", "demon", "store", "..."]
 
 word = random.randint(0, 19)
 random_word = Words[word]
 print(random_word)
 guess = 6
+guessed_letters = []
 
-if guess > 0:
-    if Words == "bepis":
-        input("Guess a letter ")
