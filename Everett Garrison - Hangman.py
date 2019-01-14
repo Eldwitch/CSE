24aa4d6b16ff5034ea25813c0bedf1168a4ada58
Eldwitch <42391782+Eldwitch@users.noreply.github.com>
2019-01-14 14:37:41 -0800

    why does it work

--- Everett Garrison - Hangman.py
+++ Everett Garrison - Hangman.py
@@ -1,18 +1,30 @@
 import random
+import string
 Words = ["singular", "bees", "SCREAMING", "satan", "god", "hello",
          "why", "seven", "zoink", "taste", "swivel", "egg", "thirsty", "thirteen",
          "joke", "demon", "store", "..."]
 
+win = 0
 word = random.choice(Words)
+word_list = list(word)
 print(word)
 guess = 6
+alphabet = list(string.printable)
 guessed_letters = []
 
-for i in range(len(word)):
-    if word[i] == word:
-        word.pop(i)
-        word.insert(i, "*")
 
-guess1 = input("Guess: ")
-guessed_letters.append(guess1)
-print(guessed_letters)
+for i in range(len(word)):
+    if word[i] in alphabet:
+        word_list.pop(i)
+        word_list.insert(i, "*")
+print("".join(word_list))
+
+while guess >= 0:
+    guess1 = input("Guess: ")
+    guess -= 1  # fix later
+    guessed_letters.append(guess1)
+    for i in range(len(word)):
+        if word[i] in guessed_letters:
+            word_list.pop(i)
+            word_list.insert(i, word[i])
+    print("".join(word_list))
