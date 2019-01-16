bd514d3be28aef75e140d70597480d47375e4aae
Eldwitch <42391782+Eldwitch@users.noreply.github.com>
2019-01-16 14:36:15 -0800

    sd'gb tklrfdnjlb

--- Everett Garrison - Hangman.py
+++ Everett Garrison - Hangman.py
@@ -4,7 +4,7 @@ Words = ["singular", "bees", "SCREAMING", "satan", "god", "hello",
          "why", "seven", "zoinks", "taste", "swivel", "egg", "thirsty", "thirteen",
          "joke", "demon", "store", "what..?", "communism", "Hello World", "Everett"]
 
-win = 0
+win = False
 word = random.choice(Words)
 word_list = list(word)
 print(word)
@@ -18,14 +18,17 @@ for i in range(len(word)):
         word_list.insert(i, "*")
 print("".join(word_list))
 
-while guess > 0 and win is not 1:
+while guess > 0 and not win:
     guess1 = input("Guess: ")
     print(guess)
-    guessed_letters.append(guess1)
+    guessed_letters.append(guess1.lower())
     for i in range(len(word)):
-        if word[i] in guessed_letters:
+        if word[i].lower() in guessed_letters:
             word_list.pop(i)
             word_list.insert(i, word[i])
-    if guess1 not in word_list:
+    if guess1.lower() not in word_list and guess1.upper() not in word_list:
         guess -= 1
     print("".join(word_list))
+    if '*' not in word_list:
+        win = True
+        print("You're winner!")
