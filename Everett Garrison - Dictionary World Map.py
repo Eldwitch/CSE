42537d22f85af13c1144c562eb7a86554c83305e
Eldwitch <42391782+Eldwitch@users.noreply.github.com>
2019-01-31 14:36:21 -0800

    it just works

--- Everett Garrison - Dictionary World Map.py
+++ Everett Garrison - Dictionary World Map.py
@@ -6,7 +6,7 @@ world_map = {
             'NORTH': "PARKING_LOT"
         }
     },
-    'PARKING LOT': {
+    'PARKING_LOT': {
         'NAME': "A Parking Lot",
         'DESCRIPTION': "There are a few cars parked here.",
         'PATHS': {
@@ -14,3 +14,24 @@ world_map = {
         }
     }
 }
+
+# Other Variables
+directions = ["NORTH", "SOUTH", "EAST", "WEST", "UP", "DOWN"]
+current_node = world_map['R19A']  # This is your current location
+playing = True
+
+# Controller
+while playing:
+    print(current_node['NAME'])
+
+    command = input(">_")
+    if command.lower() in ('q', 'quit', 'exit'):
+        playing = False
+    elif command in directions:
+        try:
+            room_name = current_node["PATHS"][command]
+            current_node = world_map[room_name]
+        except KeyError:
+            print("I can't go that way.")
+    else:
+        print("Command not recognized.")
