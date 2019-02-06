7f9eef6482e693cea6375827d17ba2ce60c76ab0
Eldwitch <42391782+Eldwitch@users.noreply.github.com>
2019-02-06 14:15:39 -0800

    some area mang

--- Everett Garrison - Dictionary World Map.py
+++ Everett Garrison - Dictionary World Map.py
@@ -1,23 +1,87 @@
 world_map = {
-    'R19A': {
-        'NAME': "Mr. Wiebes' room",
-        'DESCRIPTION': "This is the room that you are in.",
+    'FSTART': {
+        'NAME': "Forest Start",
+        'DESCRIPTION': "The beginning of your journey, where it all began. There's trees around you as far as the eye"
+                       "\n can see, and right above you the trees open up to allow a ray of sunshine to shine onto a"
+                       "\n pentagram you stand on.",
         'PATHS': {
-            'NORTH': "PARKING_LOT"
+            'NORTH': "F1",
+            'EAST': "F2",
+            'SOUTH': "F3",
+            "WEST": "F4"
         }
     },
-    'PARKING_LOT': {
-        'NAME': "A Parking Lot",
-        'DESCRIPTION': "There are a few cars parked here.",
+    'F1': {
+        'NAME': "Forest clearing",
+        'DESCRIPTION': "There's a mutated rat you cannot fight yet here because the dev is lazy",
         'PATHS': {
-            'SOUTH': "R19A"
+            'SOUTH': "FSTART",
+            'NORTH': "F14",
+            'EAST': "F2",
+            "WEST": "F4"
+        }
+    },
+    'F2': {
+        'NAME': "Forest clearing",
+        'DESCRIPTION': "There's a mutated rat you cannot fight yet here because the dev is lazy",
+        'PATHS': {
+            'NORTH': "F1",
+            'EAST': "F5",
+            'SOUTH': "F3",
+            "WEST": "FSTART"
+        }
+    },
+    'F3': {
+        'NAME': "Forest clearing",
+        'DESCRIPTION': "There's a mutated rat you cannot fight yet here because the dev is lazy",
+        'PATHS': {
+            'NORTH': "FSTART",
+            'EAST': "F2",
+            'SOUTH': "F7",
+            "WEST": "F4"
+        }
+    },
+    'F4': {
+        'NAME': "Forest clearing",
+        'DESCRIPTION': "There's a mutated rat you cannot fight yet here because the dev is lazy",
+        'PATHS': {
+            'NORTH': "F1",
+            'EAST': "FSTART",
+            'SOUTH': "F3",
+            "WEST": "F10"
+        }
+    },
+    'F5': {
+        'NAME': "A calm forest clearing",
+        'DESCRIPTION': "You look around and feel an odd sense of calm here, as if you're safe for now.",
+        'PATHS': {
+            'EAST': "F6",
+            "WEST": "F2"
+        }
+    },
+    'F6': {
+        'NAME': "A dead end.",
+        'DESCRIPTION': "A giant, wide tree blocks your path forward. Luckily a giant stick lies on the ground that"
+                       "\n looks just big enough for you to swing like a weapon.",
+        'PATHS': {
+            "WEST": "F5"
+        }
+    },
+    'F7': {
+        'NAME': "Forest clearing",
+        'DESCRIPTION': "There's a mutated rat you cannot fight yet here",
+        'PATHS': {
+            'NORTH': "F1",
+            'EAST': "F5",
+            'SOUTH': "F3",
+            "WEST": "FSTART"
         }
     }
 }
 
 # Other Variables
 directions = ["NORTH", "SOUTH", "EAST", "WEST", "UP", "DOWN"]
-current_node = world_map['R19A']  # This is your current location
+current_node = world_map['FSTART']  # This is your current location
 playing = True
 
 # Controller
