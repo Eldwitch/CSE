7608db02b4c28a0bd9c5c5e4e62383de68284fb1
Eldwitch <42391782+Eldwitch@users.noreply.github.com>
2019-03-05 14:35:28 -0800

    sivyddsgfresdgredgverdgdxgxdhbrs \]o[iupe

--- Everett Garrison - Items.py
+++ Everett Garrison - Items.py
@@ -9,7 +9,10 @@ class Weapon(Item):
         self.weapon_damage = 5
 
     def equip(self):
-        print("You wield the weapon.")
+        print("You equip the weapon.")
+
+    def unequip(self):
+        print("You unequip your weapon.")
 
     def attack(self):
         print("You attack with the weapon.")
@@ -23,11 +26,62 @@ class Armor(Item):
     def equip(self):
         print("You put on the armor.")
 
+    def unequip(self):
+        print("You unequip your armor.")
 
 class Consumable(Item):
     def __init__(self, name):
         super(Consumable, self).__init__(name)
-        self.type = 1  # Make 3 potion types, one heals, one modifies something, and the last one does something else.
+        self.type = 1  # Make multiple item types like keys, potions, etc.
 
     def consume(self):
         print("You consume the item.")
+
+
+class Makeshiftsword(Weapon):
+    def __init__(self):
+        super(Makeshiftsword, self).__init__("Makeshift sword")
+
+    def equip(self):
+        print("You equip the Makeshift Sword.")
+
+
+class Gianthand(Weapon):
+    def __init__(self):
+        super(Gianthand, self).__init__("Giant's hand")
+
+    def equip(self):
+        print("You equip the Giant's hand.")
+
+
+class Makeshiftarmor(Armor)
+    def __init__(self):
+        super(Makeshiftarmor, self).__init__("Makeshift armor")
+
+    def equip(self):
+        print("You equip the Makeshift armor.")
+
+
+class Giantskin(Armor):
+    def __init__(self):
+        super(Giantskin, self).__init__("Giant's skin")
+
+    def equip(self):
+        print("You equip the Giant's skin.")
+
+
+class Healthpotion(Consumable):
+    def __init__(self):
+        super(Healthpotion, self).__init__("Health potion")
+
+    def consume(self):
+        print("You drink the Health potion and restore ___ health.")
+
+
+class Cavekey(Consumable):
+    def __init__(self):
+        super(Cavekey, self).__init__("Ominous Cave Key")
+
+    def consume(self):
+        print("You think really hard about using the key and it flies out of your hand and into the lock on the "
+              "cave's lock, turning the key and opening the cave for you.")
