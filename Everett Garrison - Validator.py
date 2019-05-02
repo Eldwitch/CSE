14133b9010bacbe86c2c295b18553b4c170c8061
Eldwitch <42391782+Eldwitch@users.noreply.github.com>
2019-05-02 14:27:28 -0700

    skdvbkdcfb

--- Everett Garrison - Validator.py
+++ Everett Garrison - Validator.py
@@ -1,19 +1,21 @@
 import csv
 
 test_num = "4556737586899855"
+test_num2 = "8148064838470700"
 end_digit = 0
 
 
 def validate(num: str):
     last_num = int(num[15])
     next_num = drop_last_digit(num)
-    sum = odd_numbers_stuff(str(next_num))
+    # print(next_num)
+    # print(str(next_num))
+    sum = odd_numbers_stuff(''.join(next_num))
     return sum == last_num
 
 
 def drop_last_digit(num: str):
-    new_num = int(num[14::-1])  # The number that it processes but without the last digit
-    print(new_num)
+    new_num = list(num[14::-1])  # The number that it processes but without the last digit
     return new_num
 
 
@@ -25,15 +27,12 @@ def odd_numbers_stuff(num: str):
 
     for index in range(1, 15, 2):
         even_numbers.append(int(num[index]))
-    print(even_numbers)
-    print(odd_numbers)
 
     for i in range(len(odd_numbers)):
         odd_numbers[i] *= 2
         if odd_numbers[i] > 9:
             odd_numbers[i] -= 9
     all_nums = even_numbers + odd_numbers
-    print(all_nums)
     sum = 0
     for i in all_nums:
         sum += i
@@ -43,3 +42,19 @@ def odd_numbers_stuff(num: str):
 
 
 print(validate(test_num))
+
+print(validate(test_num2))
+
+
+with open("Book1.csv", 'r') as old_csv:
+    with open('MyNewFile.csv', 'w', newline='') as new_csv:
+        reader = csv.reader(old_csv)
+        writer = csv.writer(new_csv)
+        print("Processing...")
+
+        for row in reader:
+            number = row[0]  # This is a string
+            print(number)
+            if validate(number):
+                writer.writerow(row)
+    print("Done!")
